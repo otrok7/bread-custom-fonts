31d581ce096c9c7536a762e630cbb318547a606b
otrok7 <50595291+otrok7@users.noreply.github.com>
2026-06-10 17:33:51 +0200

    Initial commit

---
+++ includes/class-bread-roboto-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bmlt.app
+ * @since      1.0.0
+ *
+ * @package    Bread_Roboto
+ * @subpackage Bread_Roboto/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Bread_Roboto
+ * @subpackage Bread_Roboto/includes
+ * @author     Ron Barack <otrok7@yahoo.com>
+ */
+class Bread_Roboto_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
